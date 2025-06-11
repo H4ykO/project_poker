f8a620044e3c974258cec8383e225372ebebaab5
H4ykO <melo.jl@hotmail.com>
2025-06-10 21:05:37 -0300

    first

---
+++ poker.py
@@ -0,0 +1,42 @@
+import random
+from collections import Counter
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f"{self.rank} {self.suit}"
+    
+class Deck:
+    def __init__(self):
+        suits = ['♦', '♠', '♥', '♣']
+        ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
+        random.shuffle(self.cards)
+
+    def deal(self, num_cards):
+        return [self.cards.pop() for _ in range(num_cards)]
+    
+deck = Deck()
+player_hand = deck.deal(2)
+flop = deck.deal(3)
+turn = deck.deal(1)
+river = deck.deal(1)
+
+print("Hand: ")
+for card in player_hand:
+    print(card)
+
+print("Flop: ")
+for card in flop:
+    print(card)
+
+print("Turn: ")
+for card in turn:
+    print(card)
+
+print("River: ")
+for card in river:
+    print(card)
